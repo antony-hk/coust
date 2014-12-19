b98639544238a8035f7e37a33042af8cbbf6b4d9
Antony Tse <tseantony@gmail.com>
2014-12-20 01:10:28 +0800

    UI changed, jQuery UI css changed to local copy.

--- timetable.js
+++ timetable.js
@@ -37,7 +37,8 @@ $( document ).ready(function() {
                 $(this).autocomplete("search", $(this).val());
             });
             // add term info and last update
-            $("#termInfo").append("<p><b>Term: </b>"+terms["current"]["text"]+" <span style='font-size:0.8em;color:gray;'>(Last Update: "+data["lastUpdated"]+")</span></p>");
+            //$("#termInfo").append("<p><b>Term: </b>"+terms["current"]["text"]+" <span style='font-size:0.8em;color:gray;'>(Last Update: "+data["lastUpdated"]+")</span></p>");
+			$("#termInfo").html(terms["current"]["text"]);
             // load courses added from cookies
             loadFromCookie();
         });
