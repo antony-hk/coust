ddda72d8f077ee2b03364ed2848ff93b5b4ddc06
Antony Tse <tseantony@gmail.com>
2015-02-02 18:33:32 +0800

    UI Changed

---
+++ color.php
@@ -0,0 +1,25 @@
+<?php
+header("Content-Type: text/css");
+$bgColor = array(
+	array('FEC','FC9','EED','FFE','F73','E86','EBB','8EF','9FB','FCF')
+);
+$borderColor = array(
+	array('544','543','432','554','521','422','433','245','353','535')
+);
+$textColor = array(
+	array('544','543','432','554','521','422','433','245','353','535')
+);
+$colorChosen = rand(0, count($color)-1);
+foreach($bgColor[$colorChosen] as $i => $bgColorCode) {
+	echo ".color{$i} {";
+		echo "background: #{$bgColorCode};";
+	echo "}\n";
+	echo "div.color{$i} {";
+		echo "border: 2px solid #{$borderColor[$colorChosen][$i]};";
+		echo "color: #{$textColor[$colorChosen][$i]};";
+	echo "}\n";
+	echo "tr.color{$i} {";
+		echo "color: #{$textColor[$colorChosen][$i]};";
+	echo "}\n";
+}
+?>
