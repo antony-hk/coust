3b6a14569d3194e310515f57bf84273384b241c7
Antony Tse <tseantony@gmail.com>
2018-04-18 01:08:17 +0800

    Use React to render header

---
+++ client/src/component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header';
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+            </React.Fragment>
+        );
+    }
+}
