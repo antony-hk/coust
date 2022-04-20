b7c77bf501cdb4d42b39d1309fae45c20fe5ae00
Antony <tseantony@gmail.com>
2022-04-21 00:02:29 +0100

    Use `createRoot` from React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -8,11 +8,12 @@ import * as serviceWorker from './serviceWorker';
 // Redux
 import store from './store';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
     <Provider store={store}>
         <App />
     </Provider>
-, document.getElementById('root'));
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
