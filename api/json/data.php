9730119e91ce340868baaf5479645b92ab299bb6
Antony <tseantony@gmail.com>
2023-11-04 23:30:40 +0000

    Support non-current semesters in API

--- api/json/data.php
+++ api/json/data.php
@@ -1,5 +1,11 @@
 <?php
     header("Access-Control-Allow-Origin: *");
-    $output = json_decode(file_get_contents("../data/courseInfo.json"));
+
+    if (isset($_GET["term"]) && preg_match("/^\d{4}$/", $_GET["term"])) {
+        $output = json_decode(file_get_contents("../data/courseInfo_" . $_GET["term"] . ".json"));
+    } else {
+        $output = json_decode(file_get_contents("../data/courseInfo.json"));
+    }
+
     print json_encode($output);
 ?>
--- api/json/mkdata.php
+++ api/json/mkdata.php
@@ -4,19 +4,27 @@ include_once("parser.inc.php");
 
 $parser = new Parser();
 $result = $parser->parseCoursePage();
+
+if (isset($_GET["term"]) && preg_match("/^\d{4}$/", $_GET["term"])) {
+    $term = $_GET["term"];
+} else {
+    $term = $result["terms"]["current"]["num"];
+}
+
 $courseInfo = array();
 
 $courseInfo["terms"] = $result["terms"];
 
 foreach($result['depts'] as $dept) {
-	$data = $parser->parseCoursePage("https://w5.ab.ust.hk".$result['terms'][0]['href']."subject/".$dept);
-	foreach($data['courses'] as $course) {
-		$courseInfo[$course->code] =  $course;
-	}
+    $url =  "https://w5.ab.ust.hk/wcq/cgi-bin/" . $term . "/subject/" . $dept;
+    $data = $parser->parseCoursePage($url);
+    foreach($data['courses'] as $course) {
+        $courseInfo[$course->code] =  $course;
+    }
 }
 $last_updated = date("j F, Y, g:i a");
 $courseInfo["lastUpdated"] = $last_updated;
-file_put_contents("../data/courseInfo.json", json_encode($courseInfo));
+file_put_contents("../data/courseInfo_${term}.json", json_encode($courseInfo));
 
 print "DONE";
 
